81722e476667f4c0310bf68dd17813d5f8bb42c7
Fredrik Solås Arnstad <fredrikarnstad@MacBook-Air-Fredrik.local>
2026-01-19 14:54:26 +0100

    sha

--- Casks/font-cmu.rb
+++ Casks/font-cmu.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "font-cmu" do
   version "0.0.1"
-  sha256 "6ed3c9e6ea856bcef12ec523ca45eb7eb026a8c89fa12db6df138a60b53caccb"
+  sha256 "67b57892b8afecfc2443c22976148117ceee8737c7d2b2bd1a8e410aecdff241"
 
   url "https://github.com/ArnstadFredrik/cmu-font/archive/refs/tags/font-cmu-#{version}.zip"
   name "font-cmu"

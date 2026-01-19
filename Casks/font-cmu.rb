45ef127d4e43f45b67e4ef35865e9697c0eb2423
Fredrik Solås Arnstad <fredrikarnstad@MacBook-Air-Fredrik.local>
2026-01-19 14:45:37 +0100

    remove livecheck

--- Casks/font-cmu.rb
+++ Casks/font-cmu.rb
@@ -5,16 +5,12 @@ cask "font-cmu" do
   version "0.0.1"
   sha256 "6ed3c9e6ea856bcef12ec523ca45eb7eb026a8c89fa12db6df138a60b53caccb"
 
-  url "https://github.com/ArnstadFredrik/cmu-font/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/ArnstadFredrik/cmu-font/archive/refs/tags/font-cmu-#{version}.zip"
   name "font-cmu"
   desc ""
   homepage ""
 
   # Documentation: https://docs.brew.sh/Brew-Livecheck
-  livecheck do
-    url ""
-    strategy ""
-  end
 
   font "cmunbi.otf"
   font "cmunbx.otf"
